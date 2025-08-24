3075d0b693c5e5b6483d6a22c16192f3713625d5
herazocatalina77-svg <herazocatalina77@gmail.com>
2025-08-24 10:29:39 -0500

    Add files via upload

---
+++ Programtallercondicionales.cs
@@ -0,0 +1,32 @@
+﻿// See 
+static void Main(string[] args)
+{
+    int num1 = 0;
+    int num2 = 0;
+    int num3 = 0;
+
+    Console.WriteLine("Ingrese el numero 1:");
+    num1=Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Ingrese el numero 2:");
+    num2=Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Ingrese el numero 3:");
+    num3=Convert.ToInt32(Console.ReadLine());
+
+    if (num1 > num2)
+    {
+        if (num1 < num3)
+        {
+            Console.WriteLine($"El numero {num1}, es el mayor");
+        }
+        else
+        {
+            {
+                Console.WriteLine($"el numero {num3}, es el mayor");
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine($"el numero {num2}, es el mayor");
+    }
+}
